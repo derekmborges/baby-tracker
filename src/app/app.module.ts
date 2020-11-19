6165f322e2c2b513a807c5b783c89a8e7a4c0f63
Derek Borges <dborges2143@gmail.com>
2020-11-19 00:28:57 -0500

    Add iOS and lock screen to portrait

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -36,7 +37,8 @@ import { SleepEditModalComponent } from './sleep/sleep-edit-modal/sleep-edit-mod
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    StorageService
+    StorageService,
+    ScreenOrientation
   ],
   bootstrap: [AppComponent]
 })
